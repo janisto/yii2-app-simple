8f0652256406efe5f0609ce181ecf58cdcc40c5f
Jani Mikkonen <jani.mikkonen@whitesheep.fi>
2014-10-22 14:59:41 +0300

    Update vendor paths

--- protected/tests/codeception/bin/_bootstrap.php
+++ protected/tests/codeception/bin/_bootstrap.php
@@ -8,7 +8,7 @@ defined('STDOUT') or define('STDOUT', fopen('php://stdout', 'w'));
 
 defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(dirname(dirname(__DIR__))));
 
-require(YII_APP_BASE_PATH . '/vendor/autoload.php');
-require(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
+require(YII_APP_BASE_PATH . '/../vendor/autoload.php');
+require(YII_APP_BASE_PATH . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@tests', dirname(dirname(__DIR__)));
