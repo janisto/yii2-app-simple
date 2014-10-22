79ef7ee7dfa2518ba3fb0207cce1cbd5148371ab
Jani Mikkonen <jani.mikkonen@whitesheep.fi>
2014-10-22 13:58:15 +0300

    Add modified basic template

---
+++ protected/config/console.php
@@ -0,0 +1,32 @@
+<?php
+
+Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
+
+$params = require(__DIR__ . '/params.php');
+$db = require(__DIR__ . '/db.php');
+
+return [
+    'id' => 'simple-console',
+    'basePath' => dirname(__DIR__),
+    'vendorPath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .'vendor',
+    'bootstrap' => ['log', 'gii'],
+    'controllerNamespace' => 'app\commands',
+    'modules' => [
+        'gii' => 'yii\gii\Module',
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+    ],
+    'params' => $params,
+];
